a4e34df98c5542632944486b7438e5ff9fb2f97f
Ehsan ul khaliq <137249680+Ehsanulkhaliq4@users.noreply.github.com>
2025-11-22 22:40:40 +0500

    Working on keys managment

---
+++ src/enviornments/enviornments.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+   openAIApiKey: '{openAIApiKey}', // Use dummy key for development
+   openAIApiUrl: 'https://api.openai.com/v1/chat/completions'
+};
