9fda6ef873a5695eebbcf02c9444a939d5cd8238
Ehsanulkhaliq <ehsanulkhaliq274@gmail.com>
2025-12-22 17:34:02 +0500

    Environment-Variable working

--- src/enviornments/enviornments.prod.ts
+++ src/enviornments/enviornments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   openAIApiKey: '{OPEN_API_KEY}', // Use dummy key for development
-   openAIApiUrl: 'https://api.openai.com/v1/chat/completions'
+  openAIApiKey: 'openAIApiKey', // Use dummy key for development
+  openAIApiUrl: 'https://api.openai.com/v1/chat/completions',
 };
