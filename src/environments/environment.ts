b42b9f0e869b2a21be8758e0ac6096cf07d4ffe4
Ehsanulkhaliq <ehsanulkhaliq274@gmail.com>
2025-12-23 09:56:38 +0500

    Folder file name Update

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  openAIApiKey: 'NG_APP_API_KEY_PLACEHOLDER', // Use dummy key for development
+  openAIApiUrl: 'https://api.openai.com/v1/chat/completions',
+};
